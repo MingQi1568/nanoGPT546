3600533ca9721415009014b9c10bfddea574c4f1
theKevKev <kevinhan.119@gmail.com>
2025-12-15 20:17:34 -0500

    grad accum steps = 8

--- config/train_546.py
+++ config/train_546.py
@@ -14,7 +14,7 @@ wandb_project = '546-char'
 wandb_run_name = 'mini-gpt'
 
 dataset = 'ese546data'
-gradient_accumulation_steps = 1
+gradient_accumulation_steps = 8
 batch_size = 64
 block_size = 256 # context of up to 256 previous characters
 
